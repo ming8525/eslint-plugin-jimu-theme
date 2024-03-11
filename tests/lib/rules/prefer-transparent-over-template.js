f623fcdc5cb90bae9d9d55e1613c6e3ba9330bc8
Mingyang Han <Mingyang_Han@esri.com>
2024-03-11 11:05:01 +0800

    feat: add new rule prefer-transparent-over-template

---
+++ tests/lib/rules/prefer-transparent-over-template.js
@@ -0,0 +1,17 @@
+const { ruleTester } = require('../utils');
+const rule = require("../../../lib/rules/prefer-transparent-over-template");
+
+ruleTester.run("prefer-transparent-over-template", rule, {
+  valid: [
+    {
+      code: "const style = `color: transparent;`"
+    }
+  ],
+  invalid: [
+    {
+      code: "const style = `color: ${'transparent'};`",
+      output: "const style = `color: transparent;`",
+      errors: [{ messageId: "message", type: "Literal" }]
+    }
+  ],
+});
