7801a82328b1237c3860ad406c3fdca8fbdeaa36
Mingyang Han <Mingyang_Han@esri.com>
2024-11-21 10:06:08 +0800

    test: add unit tests for json support

--- tests/lib/utils.js
+++ tests/lib/utils.js
@@ -8,11 +8,12 @@ const ruleTester = new RuleTester({
     parserOptions: {
       parser: '@typescript-eslint/parser',
       projectService: {
-        allowDefaultProject: ['*.js', 'project/builder/*.js', 'project/app/*.js'],
+        allowDefaultProject: ['*.js', 'project/builder/*.{js,json}', 'project/test/*.{js,json}'],
       },
       ecmaFeatures: {
         jsx: true
       },
+      extraFileExtensions: ['.json'],
       ecmaVersion: 6,
       tsconfigRootDir: path.join(__dirname, '../../fixture'),
       allowAutomaticSingleRunInference: true,
